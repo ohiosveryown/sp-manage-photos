392553b1da32ad520dad7d5b10ad67eb50cb14c1
ohiosveryown <matt@ohiosveryown.co>
2018-06-27 10:36:00 -0400

    Reset and Default Styles

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+import '@/assets/style/reset.scss'
+import '@/assets/style/var.scss'
+import '@/assets/style/util.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#galleries')
